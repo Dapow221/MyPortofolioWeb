936f26eb9352942a5e55a28342620ebc7547c869
Dapow221 <141142350+Dapow221@users.noreply.github.com>
2024-10-13 09:23:14 +0700

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class Document extends NextDocument {
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
-          <NextScript />
+          <NextScript />     
         </body>
       </Html>
     );
